e772f75a406ea05d097ee52bebc61d9c171f90bf
nicholvj <nicholvj@stud.ntnu.no>
2018-10-25 21:18:19 +0200

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-import bodyParser from 'body-parser'
 import express from 'express'
+import bodyParser from 'body-parser'
+import { router } from './routes/movies'
+
 const app = express()
+
+// Define bodyParse middleware to be used
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-const router = express.Router()
-router.get('/cities', (req, res) => {
-  const cities = [
-    {name: 'New York City', population: 8175133},
-    {name: 'Los Angeles',   population: 3792621},
-    {name: 'Chicago',       population: 2695598}
-  ]
-  res.json(cities)
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
 })
-app.use(router)
-app.set('port', (process.env.PORT || 5000))
+
+app.use('/api', router)
+app.set('port', (process.env.PORT || 5006))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
 })
