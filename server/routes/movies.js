08ff94d7320153fdea64426d6e040877e4ae30ad
nicholvj <nicholvj@stud.ntnu.no>
2018-10-26 10:28:41 +0200

    Added restful error handling for the movies endpoint

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -8,14 +8,20 @@ router.get('/movies', (req, res) => {
   if (req.query.search) {
     query = {
       text: 'SELECT * FROM movie WHERE title LIKE $1',
-      values: [`%${req.query.search.toString()}%`]
+      values: [`%${req.query.search}%`]
     }
   } else {
     query = 'SELECT * FROM movie'
   }
   pool.query(query)
-    .then((movies) => res.json(movies.rows))
-    .catch(error => console.error('Error executing query', error.stack))
+    .then((movies) => {
+      movies.rows.length > 0 ? res.json(movies.rows) : res.status(404).json(
+        generateError(400, 'No movies matched your search query'))
+    })
+    .catch(() => res.status(500).json(generateError(500, 'Internal server error')))
 })
 
+function generateError (status, errorMessage) {
+  return { status: status, error: errorMessage }
+}
 export { router }
