e9af81897e8ca9154a015f11b0de7e87cb535be4
nicholvj <nicholvj@stud.ntnu.no>
2018-10-28 20:43:45 +0100

    Added error handling for pool errors and a close method for server which ends the pool on close

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -5,4 +5,9 @@ const config = JSON.parse(fs.readFileSync('db_config.json', 'utf8'))
 
 const pool = new Pool(config)
 
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
 export { pool }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { router } from './routes/movies'
+import { pool } from './dbConnect'
 
 const app = express()
 
@@ -13,9 +14,13 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', router)
-app.set('port', (process.env.PORT || 5020))
+app.set('port', (process.env.PORT || 5021))
 const server = app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
 })
 
-export { server }
+function close () {
+  pool.end()
+  server.close()
+}
+export { close, server }
