3f872d61cf9312dd6f1f389ba3f7e9b41f483871
nicholvj <nicholvj@stud.ntnu.no>
2018-10-29 14:20:34 +0100

    Added movie object test

--- server/__tests__/routes/movies.test.js
+++ server/__tests__/routes/movies.test.js
@@ -1,8 +1,9 @@
 import request from 'supertest'
 import { close, server } from '../../server'
+import Joi from 'joi'
 
 describe('GET /api/movies - get movies', () => {
-  afterAll(() => close())
+  afterAll(() => close().then(() => console.log('Pool ended and server')))
 
   it('should return json and 200 status code', (done) => {
     return request(server)
@@ -11,11 +12,25 @@ describe('GET /api/movies - get movies', () => {
       .expect('Content-Type', /json/, done)
   })
 
-  it('should return all movies', () => {
-    request(server)
+  it('should return a movie object', (done) => {
+    const movie = {
+      movie_id: Joi.number(),
+      title: Joi.string(),
+      summary: Joi.string(),
+      popularity: Joi.string(),
+      language: Joi.string(),
+      release_date: Joi.string(),
+      poster_path: Joi.string(),
+      comment: Joi.string().allow(''),
+      rating: Joi.string()
+    }
+
+    return request(server)
       .get('/api/movies')
       .then(response => {
-        expect(response.body).toHaveLength(508)
+        const { error } = Joi.validate(response.body[0], movie)
+        expect(error).toEqual(null)
+        done()
       })
   })
 })
