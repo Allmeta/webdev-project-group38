f7243c5cf06acee3c7ba4c72fdf76b74384419d7
nicholvj <nicholvj@stud.ntnu.no>
2018-10-30 10:20:54 +0100

    Added tests for movies/id route

--- server/__tests__/routes/movies.test.js
+++ server/__tests__/routes/movies.test.js
@@ -5,14 +5,14 @@ import Joi from 'joi'
 describe('GET /api/movies - get movies', () => {
   afterAll(() => close().then(() => console.log('Pool ended and server')))
 
-  it('should return json and 200 status code', (done) => {
+  it('should return json and 200 status code', done => {
     return request(server)
       .get('/api/movies')
       .expect(200)
       .expect('Content-Type', /json/, done)
   })
 
-  it('should return a movie object', (done) => {
+  it('should return a movie object', done => {
     const movie = {
       movie_id: Joi.number(),
       title: Joi.string(),
@@ -29,6 +29,27 @@ describe('GET /api/movies - get movies', () => {
       .get('/api/movies')
       .then(response => {
         const { error } = Joi.validate(response.body[0], movie)
+        expect(error).toEqual(null)
+        done()
+      })
+  })
+
+  it('should return 404 error', done => {
+    return request(server)
+      .get('/api/movies/1')
+      .expect(404, done)
+  })
+
+  it('should return json error object', done => {
+    const errorObj = {
+      status: Joi.number(),
+      error: Joi.string()
+    }
+    return request(server)
+      .get('/api/movies/1')
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const { error } = Joi.validate(response.body[0], errorObj)
         expect(error).toEqual(null)
         done()
       })
