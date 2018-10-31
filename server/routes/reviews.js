6534c5bf0a0d750336f0e6101d9135614c550fdc
nicholvj <nicholvj@stud.ntnu.no>
2018-10-31 13:06:04 +0100

    Implemented reviews endpoint

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -13,17 +13,48 @@ reviews.get('/reviews', (req, res) => {
     })
 })
 
+reviews.get('/reviews/id/:id', (req, res) => {
+  if (req.params.id === undefined) return res.json(generateError(400, 'No id provided'))
+
+  const query = {
+    text: 'SELECT comment FROM movie WHERE movie_id = $1',
+    values: [`${req.params.id}`]
+  }
+  pool.query(query)
+    .then(reviews => reviews.rows.length === 1 ? res.json(reviews.rows[0]) : res.status(404).json(
+      generateError(404, 'No reviews matched the given id'))
+    )
+    .catch(() => res.status(500).json(generateError(500, 'Internal server error')))
+})
+
 reviews.post('/reviews', (req, res) => {
   const reviewObj = {
     id: Joi.number(),
     review: Joi.string()
   }
 
-  const { error, value } = Joi.validate(req.body, reviewObj)
+  const { error } = Joi.validate(req.body, reviewObj)
 
-  if (error) return res.status(400).send(error)
+  if (error) {
+    const errorMessages = error.details.map(obj => obj.message)
+    const errorObj = {
+      status: 400,
+      error: errorMessages
+    }
+    return res.status(400).json(errorObj)
+  }
 
-  res.json(value)
+  const query = {
+    text: 'UPDATE movie SET comment = $1 WHERE movie_id = $2',
+    values: [req.body.review, req.body.id]
+  }
+
+  pool.query(query)
+    .then(response =>
+      response.rowCount === 1 ? res.json(req.body) : res.json(
+        generateError(404, 'No movies matched the given id'))
+    )
+    .catch(() => res.status(500).json(generateError(500, 'Internal server error')))
 })
 
 export { reviews }
