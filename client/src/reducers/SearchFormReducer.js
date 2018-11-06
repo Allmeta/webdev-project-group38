3efec11a1265406fb896cb39a46955c26e6828b8
Fredrik Jenssen <fredjen.contactor@gmail.com>
2018-11-06 10:52:41 +0100

    Move SearchForm reducer and store to seperate files

---
+++ client/src/reducers/SearchFormReducer.js
@@ -0,0 +1,29 @@
+import {SUBMIT_SEARCH, UPDATE_GENRE, UPDATE_TITLE} from "../actions/SearchFormActionTypes";
+
+const initialState = {
+  title: '',
+  genre: '',
+  submittedTitle: '',
+  submittedGenre: '',
+};
+
+export const SearchFormReducer = (state = initialState, action) => {
+  console.log('reducer running', action);
+  switch(action.type){
+    case UPDATE_TITLE:
+      return Object.assign({}, state, {
+        title: action.title,
+      });
+    case UPDATE_GENRE:
+      return Object.assign({}, state, {
+        genre: action.genre,
+      });
+    case SUBMIT_SEARCH:
+      return Object.assign({}, state, {
+        submittedTitle: state.title,
+        submittedGenre: state.genre
+      });
+    default:
+      return state
+  }
+};
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,33 +1,5 @@
 import { createStore } from 'redux'
-import { SUBMIT_SEARCH, UPDATE_GENRE, UPDATE_TITLE } from "../actions/SearchFormActionTypes";
-
-const initialState = {
-  title: '',
-  genre: '',
-  submittedTitle: '',
-  submittedGenre: '',
-};
-
-export const SearchFormReducer = (state = initialState, action) => {
-  console.log('reducer running', action);
-  switch(action.type){
-    case UPDATE_TITLE:
-      return Object.assign({}, state, {
-        title: action.title,
-      });
-    case UPDATE_GENRE:
-      return Object.assign({}, state, {
-        genre: action.genre,
-      });
-    case SUBMIT_SEARCH:
-      return Object.assign({}, state, {
-        submittedTitle: state.title,
-        submittedGenre: state.genre
-      });
-    default:
-      return state
-  }
-};
+import { SearchFormReducer } from "../reducers/SearchFormReducer";
 
 const store = createStore(SearchFormReducer);
 
