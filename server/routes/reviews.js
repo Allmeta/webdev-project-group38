9314a7b9158e4b8361b6fb3c6a8c0dafb2cef129
nicholvj <nicholvj@stud.ntnu.no>
2018-11-11 13:24:40 +0100

    Refacotored route to use query builder

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import { pool } from '../dbConnect'
-import { generateError } from '../utils'
+import { generateError, buildReviewsQuery, regular, identity } from '../utils'
 import Joi from 'joi'
 
 const reviews = express.Router()
 
 reviews.get('/reviews', (req, res) => {
-  pool.query('SELECT comment FROM movie')
+  pool.query(buildReviewsQuery(null, regular, null))
     .then(response => res.json(response.rows))
     .catch(() => {
       res.status(500).json(generateError(500, 'Internal server error'))
@@ -14,11 +14,13 @@ reviews.get('/reviews', (req, res) => {
 })
 
 reviews.get('/reviews/id/:id', (req, res) => {
-  const query = {
-    text: 'SELECT comment FROM movie WHERE movie_id = $1',
-    values: [`${req.params.id}`]
-  }
-  pool.query(query)
+  pool.query(
+    buildReviewsQuery(
+      'SELECT comment from movie WHERE movie_id = $1',
+      regular,
+      req.params.id
+    )
+  )
     .then(reviews => reviews.rows.length === 1 ? res.json(reviews.rows[0]) : res.status(404).json(
       generateError(404, 'No reviews matched the given id'))
     )
@@ -42,12 +44,13 @@ reviews.put('/reviews', (req, res) => {
     return res.status(400).json(errorObj)
   }
 
-  const query = {
-    text: 'UPDATE movie SET comment = $1 WHERE movie_id = $2',
-    values: [req.body.review, req.body.id]
-  }
-
-  pool.query(query)
+  pool.query(
+    buildReviewsQuery(
+      'UPDATE movie SET comment = $1 WHERE movie_id = $2',
+      identity,
+      [req.body.review, req.body.id]
+    )
+  )
     .then(response =>
       response.rowCount === 1 ? res.json(req.body) : res.json(
         generateError(404, 'No movies matched the given id'))
